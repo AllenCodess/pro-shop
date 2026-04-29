c79e3701b9dd2e78b142df93dbf27380b15ca85a
AllenCodess <Aph296@protonmail.com>
2026-04-29 19:24:31 -0400

    added some css and fixed teh navigation section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,28 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../logo/logo.png";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">ProShop </Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="ProShop logo" /> ProShop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" />
-          <Nav className="ms-auto">
-            <Nav.Link href="/cart">
-              <FaShoppingCart /> Cart
-            </Nav.Link>
-            <Nav.Link href="/login">
-              <FaUser /> Sign In
-            </Nav.Link>
-          </Nav>
+          <Navbar.Collapse id="basic-navbar-nav">
+            {/*opening tag */}
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaUser /> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          {/*closing tag*/}
         </Container>
       </Navbar>
     </header>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./bootstrap.custom/bootstrap.custom.css";
+import "./proshop-theme/proshop-theme/css/bootstrap.css";
+
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
