31e606d62738b10557efbc37278ad89287b63ac7
AllenCodess <Aph296@protonmail.com>
2026-05-03 17:02:58 -0400

    setting up the structure for the database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import products from "../backend/data/products.js";
-import connectDB from "./config/db.js";
+import connectDB from "./config/db.js"; // importing the file
 const port = process.env.PORT;
 
-connectDB();
+connectDB(); // calling the function
 
 const app = express();
 
