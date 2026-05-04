3b9d25391b4534111b2a7b9210b70b51db377935
AllenCodess <Aph296@protonmail.com>
2026-05-04 18:34:57 -0400

    linking the routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import products from "../backend/data/products.js";
 import connectDB from "./config/db.js"; // importing the file
+import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT;
 
 connectDB(); // calling the function
@@ -10,5 +11,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
