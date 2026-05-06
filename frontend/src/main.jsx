c759eba0d420d3b9a79a1a303350d80e72871702
AllenCodess <Aph296@protonmail.com>
2026-05-06 13:28:29 -0400

    create redux store and added provider to main.jsx to connect store to react

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./store.js";
 import "./bootstrap.custom/bootstrap.custom.css";
 import "./proshop-theme/proshop-theme/css/bootstrap.css";
 
@@ -10,7 +11,9 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 );
