74b7a758c5b49ee16e10d6820cf43b807927d798
AllenCodess <Aph296@protonmail.com>
2026-05-13 03:04:41 -0400

    auth-middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-// import products from "../backend/data/products.js";
-import connectDB from "./config/db.js"; // importing the file
+
+import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
 const port = process.env.PORT;
 
 connectDB(); // calling the function
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
